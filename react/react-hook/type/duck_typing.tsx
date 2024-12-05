9897742133d63ff75b5316826a9c27863ae11b73
c_shunyi <2579375554@qq.com>
2024-12-06 00:01:18 +0800

    ts-duck-typing

---
+++ react/react-hook/type/duck_typing.tsx
@@ -0,0 +1,27 @@
+// ts是鸭子类型
+// 鸭子类型：只要有这个属性，就认为是鸭子
+interface one {
+  name: string
+}
+interface two extends one {
+    id: number
+}
+
+function log(obj: one) {
+  console.log(obj);
+}
+function log2(obj: two) {
+    console.log(obj);
+  }
+
+// 传入one，不报错
+const a: one = { name: 'jack' }
+log(a)
+// 传入two，不报错
+const b: two = { name: 'tom', id: 1 }
+log(b)
+// 传入无限定，不报错
+const c = { name: 'lucy' }  
+log(c)
+const d = { name: 'lucy', id: 2 }
+log2(d)
